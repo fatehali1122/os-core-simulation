a0c0fb8f6c7d91975a318ea1bde31ebd248e4986
Zoro <ahmedjutt7415@gmail.com>
2026-01-20 21:08:09 +0500

    added funtion to return counter of allocated spaces in array

--- src/main.c
+++ src/main.c
@@ -3,8 +3,13 @@
 
 // This is a temporary main function to test the build system
 int main() {
+    printf("------Before executing-----\n");
+    printf("Counter: %d\n",getprocessTableCount());
     int processId = processCreation(1,1,1);
     printf("Process ID: %d \n", processId);
+
+    printf("------After executing-----\n");
+    printf("Counter: %d\n",getprocessTableCount());
 
     changeProcessState(1);
     changeProcessState(1);
@@ -13,6 +18,5 @@ int main() {
     changeProcessState(1);
 
     terminateProcess(1);
-    
     return 0;
 }
