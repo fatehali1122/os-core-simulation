cc3a30ced40a9d60f6f8f6110ebf56e8bf0eacbf
Zoro <ahmedjutt7415@gmail.com>
2026-01-20 06:47:59 +0500

    Module 1 2/3 task are done
    Created function in which process can be created and then stored it in array and created changeProcessState funtion to change the state of the process,we will convert from array to linked list later

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include "memory/memory_manager.h"     //Linkage
 
 // This is a temporary main function to test the build system
 int main() {
+    int result = processCreation(1,1,1);
+    printf("%d \n",result);
+
+    changeProcessState(75);
+    
     printf("Build successful! The OS Simulation is running.\n");
     return 0;
 }
---
+++ src/memory/memory_manager.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include "memory_manager.h"           //Linkage
+
+int count = 1;          //global variable for assigning unique PID
+struct PCB processTable[100];
+int processTableCount = 0;
+
+int processCreation(int priority,int burstTime,int memoryUsage)
+{
+    if(processTableCount > 100)
+    {
+        printf("Array is full \n");
+        return -1;
+    }
+    struct PCB p1;
+
+    p1.pid = count;
+    count++;
+    p1.status = NEW;
+    p1.priority = priority;
+    p1.burstTime = burstTime;
+    p1.memoryUsage = memoryUsage;
+
+    //Now storing this PCB in processTable Array
+    processTable[processTableCount] = p1;
+    processTableCount++;
+
+    return p1.pid;   
+}
+
+void changeProcessState(int pid)
+{
+    int found = 0;
+    for(int i = 0;i < processTableCount;i++)
+    {
+        if(processTable[i].pid == pid)
+        {
+            found = 1;
+            switch(processTable[i].status)
+            {
+                case NEW:
+                processTable[i].status = READY;
+                break;
+
+                case READY:
+                processTable[i].status = RUNNING;
+                break;
+                
+                case RUNNING:
+                processTable[i].status = WAITING;
+                break;
+                case WAITING:
+                for(int j = i; j < processTableCount - 1;j++)
+                {
+                    processTable[j] = processTable[j+1];
+                }
+                processTableCount--;
+                return;
+                default:
+                printf("INVALID STATE\n");
+                break;
+            }
+
+        }        
+    }
+    if(found == 0)
+    {
+        printf("Process with this pid not found\n");
+    }
+}
