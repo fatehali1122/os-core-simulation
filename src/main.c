10cdd2f066098af9ab3112faf7e01dcde00fcb7f
UbaidUllah-bit <ubaidullah9f@gmail.com>
2026-01-20 21:21:52 +0500

    Distinction in Test added in main

--- src/main.c
+++ src/main.c
@@ -4,18 +4,16 @@
 // This is a temporary main function to test the build system
 int main() {
     printf("------Before executing-----\n");
-    printf("Counter: %d\n",getprocessTableCount());
     int processId = processCreation(1,1,1);
-    printf("Process ID: %d \n", processId);
+	printf("Process ID: %d \n", processId);
+	printf("Counter: %d\n\n",getprocessTableCount());
 
     printf("------After executing-----\n");
-    printf("Counter: %d\n",getprocessTableCount());
+	terminateProcess(1);
+    printf("Counter: %d\n\n",getprocessTableCount());
 
-    changeProcessState(1);
-    changeProcessState(1);
-    changeProcessState(1);
-    changeProcessState(1);
-    changeProcessState(1);
+	printf("---Testing if process exists or not after execution---\n");
+    changeProcessState(1); //changing non-existent process state
     terminateProcess(1);
 	
     return 0;
